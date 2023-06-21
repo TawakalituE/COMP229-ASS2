fbbe3cd18763696f7f36ac2727edf0ab3dc8d6ee
TawakalituE <134027520+TawakalituE@users.noreply.github.com>
2023-06-20 22:02:29 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+/*
+File: users.js
+Student's Name: Adinlewa Tawakalitu Eunice
+Student ID: 301281523
+Date: June 4, 2023
+*/
+
+module.exports =
+{
+    "URI": "mongodb://localhost/business_store"
+}
+
+
